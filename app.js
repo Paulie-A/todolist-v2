f58fd1e8da5ebad8f82c7561b525eeefb7ef7caa
Paul August <paul.augustp@gmail.com>
2019-04-12 23:03:17 +0100

    changed db password

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-paul:test123@cluster0-vo4nh.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-paul:test-123@cluster0-vo4nh.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
