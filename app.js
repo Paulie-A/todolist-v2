c56d402a1be15fe4e45527b3cbaa718785d1edac
Paul August <paul.augustp@gmail.com>
2019-06-30 20:00:55 +0100

    corrected .env code

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-var url = process.env.MONGOLAB_URI;
+const url = process.env.MONGOLAB_URI.toString();
 // const date = require(__dirname + "/date.js");
 
 const app = express();
